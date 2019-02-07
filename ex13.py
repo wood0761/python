038066d2c65457919c4e8c2eeec1f173dd292a15
wood0761 <wood0761@gmail.com>
2019-02-07 10:43:40 -0600

    ex12 and 13

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("The script is calle:", script)
+print("Your first variable is:", first)
+print("Your secon variable is:", second)
+print("Your third varibale is:", third)
